9afaf05d5b53750c2e12905cb8f060013d2420dd
juanwisz <juanwisznia@gmail.com>
2024-02-25 00:48:49 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_package",
-    version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    name='llm_strings',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
 )
